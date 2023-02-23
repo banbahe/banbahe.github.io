2f94beb6567fbd68f05621fd8ca6d27f19a2127c
E <25061425+banbahe@users.noreply.github.com>
2023-02-23 00:45:03 -0600

    Update showtimes-global.js

--- showtimes-global.js
+++ showtimes-global.js
@@ -1,41 +1,41 @@
-﻿function ReplaceUri(args) {
+function ReplaceUri(args) {
     // BORRAR ARCHIVO
     // AGREGAR ESTE ARCHIVO EN S3 SOLO EN AMBIENTE STAGE / PREPROD
     debugger;
-	
+
     const url = new URL(args.href);
     const params = new URLSearchParams(url.search);
     const currentLocation = window.location.hostname;
-	
+
     const conditions0 = ['localhost'];
-    const conditions1 = [ 'stage'];
-	const conditions2 = ['preprod'];
+    const conditions1 = ['stage'];
+    const conditions2 = ['preprod'];
 
     const isLocal = conditions0.some(i => currentLocation.includes(i));
     const isStage = conditions1.some(i => currentLocation.includes(i));
-	const isPreprod = conditions2.some(i => currentLocation.includes(i));
+    const isPreprod = conditions2.some(i => currentLocation.includes(i));
 
     let countryCode = 'MX';
-	let hostNamePuchase = 'https://sls-stage-compra.cinepolis.com/';
+    let hostNamePuchase = 'https://sls-stage-compra.cinepolis.com/';
     let cinemaVistaId = params.get('cinemacode');
     let showtimeVistaId = params.get('txtSessionId');
-	
-	let search_countryCode_local = document.getElementById('tmp-country').value.split("'");
+
+    let search_countryCode_local = document.getElementById('tmp-country').value.split("'");
     let countryCode_local = search_countryCode_local[1];
-	
-	try {
-		
-		countryCode = countryCode_local.slice(-2);
-		cinemaVistaId == null ? params.get('cinemaVistaId');
-		showtimeVistaId == null ? params.get('showtimeVistaId');
-		
-	} catch(err) {
-		countryCode = 'MX'
-	}
-    
+
+    try {
+
+        countryCode = countryCode_local.slice(-2);
+        cinemaVistaId == null ? params.get('cinemaVistaId') : cinemaVistaId;
+        showtimeVistaId == null ? params.get('showtimeVistaId') : showtimeVistaId;
+
+    } catch (err) {
+        countryCode = 'MX'
+    }
+
     if (isPreprod) {
-		hostNamePuchase = 'https://sls-preprod-compra.cinepolis.com/';
-		cinemaVistaId = params.get('cinemaVistaId');
+        hostNamePuchase = 'https://sls-preprod-compra.cinepolis.com/';
+        cinemaVistaId = params.get('cinemaVistaId');
         showtimeVistaId = params.get('showtimeVistaId');
         // const search_countryCode = currentLocation.split('.');
         // countryCode = search_countryCode[3].toUpperCase();
@@ -56,6 +56,6 @@
             showtimeVistaId = showtimeVistaId;
     }
 
-    let merge = `${hostNamePuchase}/?cinemaVistaId=${cinemaVistaId}&showtimeVistaId=${showtimeVistaId}&countryCode=${countryCode}`;
+    let merge = `${hostNamePuchase}?cinemaVistaId=${cinemaVistaId}&showtimeVistaId=${showtimeVistaId}&countryCode=${countryCode}`;
     args.href = merge;
 }
